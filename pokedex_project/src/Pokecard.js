800613cd9c7a02df711909b677abf046622a280f
Gorav-K <78222737+Gorav-K@users.noreply.github.com>
2023-12-21 00:56:59 -0500

    added fancier images

--- pokedex_project/src/Pokecard.js
+++ pokedex_project/src/Pokecard.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
+// const POKE_API = 
+// 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'; 
 const POKE_API = 
-'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'; 
+'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'; 
 
+let padToThree= (number) =>(number <= 999 ? `00${number}`.slice(-3) : number) 
 class Pokecard extends Component {
     render() {
-        let imagSrc = `${POKE_API}${this.props.id}.png`;//this is a template string 
+        let imagSrc = `${POKE_API}${padToThree(this.props.id)}.png`;//this is a template string 
         return (
             <div className="Pokecard">
                 <h1>{this.props.name}</h1>
