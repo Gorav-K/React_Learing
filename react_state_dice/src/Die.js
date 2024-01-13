f7e5e68ec69e3fecd2a5ecb67c556fca7632eeb0
Gorav-K <78222737+Gorav-K@users.noreply.github.com>
2024-01-13 18:36:07 -0500

    add animation and css

--- react_state_dice/src/Die.js
+++ react_state_dice/src/Die.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import "./Die.css";
+import './Die.css';
 
 class Die extends Component {
     render() {
-        return (
-            <div>
-                <i class={`Die fas fa-dice-${this.props.face}`}></i>
-            </div>
-        )
+        return  <i class={`Die fas fa-dice-${this.props.face} ${this.props.isRolling? 'shaking' : ''}`} />
+        
     }
 }
 
